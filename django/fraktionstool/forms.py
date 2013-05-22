bdc7d24ef4343a9f4fdd15fedda7361e1553dc6c
Tom Kazimiers <tom@voodoo-arts.net>
2013-05-23 00:10:41 +0200

    Add Vorhaben field to selection form

--- django/fraktionstool/forms.py
+++ django/fraktionstool/forms.py
@@ -1,9 +1,11 @@
 from django import forms
-from fraktionstool.models import Gremium
+from fraktionstool.models import Gremium, Vorhaben
 
 class GremiumSelectionForm(forms.Form):
 	gremium = forms.ModelChoiceField(
 		queryset=Gremium.objects.all())
+	vorhaben = forms.ModelChoiceField(
+		queryset=Vorhaben.objects.all())
 	my_gremien = forms.BooleanField(
 		label='Nur meine Gremien',
 		required=False)
