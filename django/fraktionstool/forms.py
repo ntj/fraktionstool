e0f17421e69cdf68c97ce02a994c9e2b8a7ece87
Tom Kazimiers <tom@voodoo-arts.net>
2013-05-28 00:15:02 +0200

    Remove dashed lines for empty values in selection form

--- django/fraktionstool/forms.py
+++ django/fraktionstool/forms.py
@@ -2,9 +2,9 @@ from django import forms
 from fraktionstool.models import Gremium, Vorhaben
 
 class GremiumSelectionForm(forms.Form):
-	gremium = forms.ModelChoiceField(
+	gremium = forms.ModelChoiceField(empty_label=None,
 		queryset=Gremium.objects.all())
-	vorhaben = forms.ModelChoiceField(
+	vorhaben = forms.ModelChoiceField(empty_label=None,
 		queryset=Vorhaben.objects.all())
 	my_gremien = forms.BooleanField(
 		label='Nur meine Gremien',
