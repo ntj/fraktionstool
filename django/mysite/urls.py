4dbf1e02677ff144f26252c6d6da8ff640366acd
Antje Kazimiers <antje@allesjetzt.net>
2014-02-10 20:04:21 -0500

    Link password change site into frontend

--- django/mysite/urls.py
+++ django/mysite/urls.py
@@ -19,6 +19,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include("django.contrib.auth.urls")),
 )
 
 urlpatterns += patterns('django.contrib.auth.views',
